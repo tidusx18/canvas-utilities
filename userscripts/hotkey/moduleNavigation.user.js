64863d69e47aecc938c9e52bb832cd8aae57fbca
Matt Thomson <red.cataclysm@gmail.com>
2017-12-08 11:48:46 -0500

    Updates module navigation hotkeys

---
+++ userscripts/hotkey/moduleNavigation.user.js
@@ -0,0 +1,79 @@
+// ==UserScript==
+// @name         Canvas UX - Hotkeys: Module Navigation
+// @namespace    https://github.com/redice44
+// @version      0.0.1b
+// @description  Hotkey: Left and right arrow key navigation through module content.
+// @author       Matt Thomson
+// @include      https://[YOUR URL]/courses/*/pages/*
+// @include      https://[YOUR URL]/courses/*/assignments/*
+// @include      https://[YOUR URL]/courses/*/discussion_topics/*
+// @include      https://[YOUR URL]/courses/*/quizzes/*
+// @include      https://[YOUR URL]/courses/*/files/*
+// @include      https://[YOUR URL]/courses/*/modules/items/*
+// @exclude      https://[YOUR URL]/courses/*/pages/*/edit
+// @exclude      https://[YOUR URL]/courses/*/assignments/*/edit
+// @exclude      https://[YOUR URL]/courses/*/discussion_topics/*/edit
+// @exclude      https://[YOUR URL]/courses/*/quizzes/*/edit
+// @run-at       document-idle
+// ==/UserScript==
+
+(function() {
+
+  function hotkeyAction( event ) {
+
+    const nextElementSelector = '#content .module-sequence-footer-content > .module-sequence-footer-button--next > a';
+    const prevElementSelector = '#content .module-sequence-footer-content > .module-sequence-footer-button--previous';
+
+    switch ( event.code ) {
+
+      case 'ArrowLeft':
+
+        event.preventDefault();
+
+        try {
+
+          document.querySelector( prevElementSelector ).click();
+
+        } catch ( error ) {
+
+          // Fail silently. Likely the element hasn't loaded yet.
+
+        }
+
+        break;
+
+      case 'ArrowRight':
+
+        event.preventDefault();
+
+        try {
+
+          document.querySelector( nextElementSelector ).click();
+
+        } catch ( error ) {
+
+          // Fail silently. Likely the element hasn't loaded yet.
+
+        }
+
+        break;
+
+    }
+
+  }
+
+  function handleHotkey( event ) {
+
+    if ( !event.code ) {
+
+      return;
+
+    }
+
+    hotkeyAction( event );
+
+  }
+
+  document.addEventListener( 'keydown', handleHotkey, false );
+
+})();
---
+++ userscripts/hotkey/readme.md
@@ -0,0 +1,17 @@
+# Canvas User Scripts: Hotkeys
+
+### Module Navigation
+
+Adds arrow key navigation through module items.
+
+**Versions**
+
+- 0.0.1b
+  - Fix: URL matching and exclusions
+
+- 0.0.1a
+  - Fix: URL match for external links
+
+- 0.0.1: Initial Release
+  - Chrome v62.0 64-bit
+  - Canvas Release 2017-10-28.32
