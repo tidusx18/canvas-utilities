f6d4aaebd23471e93f8077312e06747a6de9c3ba
Matt Thomson <red.cataclysm@gmail.com>
2017-12-12 12:24:45 -0500

    Updates inclusions and exclusions for module navigation

--- userscripts/hotkey/moduleNavigation.user.js
+++ userscripts/hotkey/moduleNavigation.user.js
@@ -3,17 +3,23 @@
 // @namespace    https://github.com/redice44
 // @version      0.0.1b
 // @description  Hotkey: Left and right arrow key navigation through module content.
-// @author       Matt Thomson
-// @include      https://[YOUR URL]/courses/*/pages/*
+// @author       Matt Thomson <red.cataclysm@gmail.com>
+
 // @include      https://[YOUR URL]/courses/*/assignments/*
 // @include      https://[YOUR URL]/courses/*/discussion_topics/*
-// @include      https://[YOUR URL]/courses/*/quizzes/*
 // @include      https://[YOUR URL]/courses/*/files/*
 // @include      https://[YOUR URL]/courses/*/modules/items/*
-// @exclude      https://[YOUR URL]/courses/*/pages/*/edit
-// @exclude      https://[YOUR URL]/courses/*/assignments/*/edit
-// @exclude      https://[YOUR URL]/courses/*/discussion_topics/*/edit
-// @exclude      https://[YOUR URL]/courses/*/quizzes/*/edit
+// @include      https://[YOUR URL]/courses/*/pages/*
+// @include      https://[YOUR URL]/courses/*/quizzes/*
+
+// @exclude      https://[YOUR URL]/courses/*/assignments/*/edit*
+// @exclude      https://[YOUR URL]/courses/*/discussion_topics/*/edit*
+// @exclude      https://[YOUR URL]/courses/*/pages/*/edit*
+// @exclude      https://[YOUR URL]/courses/*/quizzes/*/edit*
+
+// @exclude      https://[YOUR URL]/courses/*/assignments/new
+// @exclude      https://[YOUR URL]/courses/*/discussion_topics/new
+
 // @run-at       document-idle
 // ==/UserScript==
 
