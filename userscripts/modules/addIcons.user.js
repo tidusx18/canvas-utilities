14604dcd85c76c9383181b5f9a2c76775d2f994d
Matt Thomson <red.cataclysm@gmail.com>
2018-02-21 09:22:30 -0500

    Adds Quick Indent Icons to Modules

---
+++ userscripts/modules/addIcons.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Canvas UX - Module: Quick Access Indent Icons
+// @namespace    https://github.com/redice44
+// @version      0.0.1
+// @description  Adds Indent Icons to Module Items
+// @author       Matt Thomson
+// @match        https://[YOUR URL]/courses/*/modules
+// @run-at       document-idle
+// ==/UserScript==
+
+
+(function() {
+
+  const moduleItems = document.querySelectorAll( 'ul.ig-list > li.context_module_item' );
+
+  for ( let i = 0; i < moduleItems.length; i++ ) {
+
+    const admin = moduleItems[ i ].querySelector( '.ig-admin' );
+    const decIndent = admin.querySelector( 'ul.al-options a.outdent_item_link' ).cloneNode( true );
+    const incIndent = admin.querySelector( 'ul.al-options a.indent_item_link' ).cloneNode( true );
+
+    decIndent.removeChild( decIndent.childNodes[ 1 ] );
+    incIndent.removeChild( incIndent.childNodes[ 1 ] );
+
+    admin.insertBefore( incIndent, admin.firstChild );
+    admin.insertBefore( decIndent, admin.firstChild );
+
+  }
+
+})();
--- userscripts/modules/readme.md
+++ userscripts/modules/readme.md
@@ -1,5 +1,15 @@
 # Canvas User Scripts: Modules
 
+### Quick Access Indent Icons
+
+Adds Indent Icons to Module Items
+
+**Versions**
+
+- 0.0.1: Initial Release
+  - Chrome v63.0 64-bit
+  - Canvas Revision 2018-02-17.16
+
 ### Add Page List Sort and Filter
 
 Sorts and filters the pages list when adding to modules.
@@ -8,18 +18,4 @@ Sorts and filters the pages list when adding to modules.
 
 - 0.0.1: Initial Release
   - Chrome v62.0 64-bit
-  - Canvas Release 2017-10-28.32
-
-
-### Hotkey Navigation
-
-Adds arrow key navigation through modules
-
-**Versions**
-
-- 0.0.1a
-  - Fix: URL match for external links
-
-- 0.0.1: Initial Release
-  - Chrome v62.0 64-bit
-  - Canvas Release 2017-10-28.32
+  - Canvas Revision 2017-10-28.32
